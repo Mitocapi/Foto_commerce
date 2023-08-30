b4a5549aef9f34675d39029b2d549aff90240e77
Mitocapi <marcocapi01@gmail.com>
2023-08-30 19:54:24 +0200

    crying over wrong redirection NOT GOOD

--- APPfoto/urls.py
+++ APPfoto/urls.py
@@ -6,8 +6,9 @@ app_name = "APPfoto"
 urlpatterns = [
     path("/", views.home_view, name="home"),
     path("ricerca/", views.search, name="cercaFoto"),
-    path("ricerca/<str:sstring>/<str:where>", views.FotoRicercaView.as_view(), name="ricerca_risultati"),
+    path("ricerca/<str:sstring>/<str:where>", views.FotoListaRicercataView.as_view(), name="ricerca_risultati"),
     path("crea_foto/", views.CreateFotoView.as_view(), name="creafoto"),
-    path("lista_foto",views.FotoListView.as_view(), name="listafoto")
-
+    path("lista_foto",views.FotoListaRicercataView.as_view(), name="listafoto"),
+    path("which_colours", views.SearchWrongColoursView.as_view(), name="search_wrong_colours"),
+    path("list", views.FotoListView.as_view(), name="listview")
 ]
--- APPfoto/views.py
+++ APPfoto/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import *
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 # Create your views here.
 
@@ -11,10 +11,32 @@ def home_view(request):
     return render(request, template_name="APPfoto/home.html")
 
 
+from .forms import SearchForm  # Import your form class
+
+from django.shortcuts import render
+from django.views import View
+
+
+class SearchWrongColoursView(View):
+    template_name = "APPfoto/search_wrong_colour.html"  # Replace with the actual template path
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    # If you want to handle POST requests as well
+    def post(self, request, *args, **kwargs):
+        # Your post logic here
+        return render(request, self.template_name)
+
+
+
 class FotoListView(ListView):
     titolo="Abbiamo trovato queste foto"
     model = Foto
-    template_name= "APPfoto/lista_foto.html"
+    template_name = "APPfoto/lista_foto.html"
+
+
+from django.contrib import messages
 
 
 def search(request):
@@ -23,25 +45,39 @@ def search(request):
         if form.is_valid():
             sstring = form.cleaned_data.get("search_string")
             where = form.cleaned_data.get("search_where")
+            print("SIAMO IN SEARCH PRIMA O DOPO WRONG?")
             return redirect("APPfoto:ricerca_risultati", sstring, where)
 
     else:
         form = SearchForm()
 
-    return render(request, template_name="APPfoto/search.html", context={"form":form})
+    return render(request, "APPfoto/search.html", {"form": form})
 
 
-class FotoRicercaView(FotoListView):
+class FotoListaRicercataView(FotoListView):
     titolo = "risultati ricerca"
+
     def get_queryset(self):
         sstring = self.request.resolver_match.kwargs["sstring"]
         where = self.request.resolver_match.kwargs["where"]
 
-        if "name" in where:
-            qq = self.model.objects.filter(titolo__icontains=sstring)
-        else:
-            qq = self.model.objects.filter(autore__icontains=sstring)
+        if where == "name":
+            qq = self.model.objects.filter(name__icontains=sstring)
+        elif where == "landscape":
+            # Filter using a boolean condition
+            qq = self.model.objects.filter(landscape=True)
+        elif where == "main_colour":
+            # Filter using elements from a list
+            COLOUR_CHOICES_to_filter = ["Black","Dark Blue","Green", "Gray", "Light Blue", "Orange", "Pink",
+                                        "Purple", "Red", "White", "Yellow"]
+            qq = self.model.objects.filter(main_colour__in=sstring)
 
+        else:
+            qq = self.model.objects.filter(artist_name__icontains=sstring)
+        if not qq:
+            print("si va?")
+            return
+        print(qq)
         return qq
 
 
